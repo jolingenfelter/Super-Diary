17a24fc58676404777ec76347f3b423b78e9f58f
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-03-01 16:21:55 -0500

    Add NewEntryVC

---
+++ SuperDiary/SuperDiary/NewEntryViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NewEntryViewController.swift
+//  SuperDiary
+//
+//  Created by Joanna Lingenfelter on 3/1/17.
+//  Copyright © 2017 JoLingenfelter. All rights reserved.
+//
+
+import UIKit
+
+class NewEntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
