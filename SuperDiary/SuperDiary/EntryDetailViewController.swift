0ee0b0e3e0e18fee9a300f104fd2e03a599d332a
Joanna Lingenfelter <lingenfelter.joanna@gmail.com>
2017-03-01 19:15:04 -0500

    Add asset images, addLocationButton, noteTextField

--- SuperDiary/SuperDiary/EntryDetailViewController.swift
+++ SuperDiary/SuperDiary/EntryDetailViewController.swift
@@ -17,7 +17,23 @@ class EntryDetailViewController: UIViewController {
     
     lazy var noteTextView: UITextView = {
         let textView = UITextView()
+        textView.layer.borderColor = UIColor.lightGray.cgColor
+        textView.layer.borderWidth = 1.5
+       
         return textView
+    }()
+    
+    lazy var addLocationButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Add location", for: .normal)
+        button.setTitleColor(.lightGray , for: .normal)
+        let geolocateImage = UIImage(named: "icn_geolocate")
+        button.setImage(geolocateImage, for: .normal)
+        button.imageEdgeInsets = UIEdgeInsetsMake(0, -25, 0, 0)
+        button.titleLabel?.textAlignment = .left
+        button.addTarget(self, action: #selector(addLocation), for: .touchUpInside)
+        
+        return button
     }()
     
     lazy var imageView: UIImageView = {
@@ -29,6 +45,8 @@ class EntryDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navbarSetup()
+        self.automaticallyAdjustsScrollViewInsets = false
+        self.view.backgroundColor = UIColor.white
         
         // Show data of pre-existing note
         noteTextView.text = entry?.note
@@ -37,6 +55,38 @@ class EntryDetailViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        view.addSubview(noteTextView)
+        noteTextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            noteTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            noteTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            noteTextView.topAnchor.constraint(equalTo: (navigationController?.navigationBar.bottomAnchor)!, constant: 20),
+            noteTextView.heightAnchor.constraint(equalToConstant: 200)])
+        
+        view.addSubview(addLocationButton)
+        addLocationButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            addLocationButton.topAnchor.constraint(equalTo: noteTextView.bottomAnchor, constant: 15),
+            addLocationButton.leadingAnchor.constraint(equalTo: noteTextView.leadingAnchor),
+            addLocationButton.heightAnchor.constraint(equalToConstant: 35),
+            addLocationButton.widthAnchor.constraint(equalToConstant: 150)])
+        
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+    }
+    
+    // MARK: - Location
+    
+    func addLocation() {
+        
     }
 }
 
